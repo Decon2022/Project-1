d4f91e1411ed1b7f025e0addd31d42f10e9fec0e
Decon2022 <danielcontreras344@gmail.com>
2025-12-26 23:24:14 -0800

    Count Words complete

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 
+
+
+'''
+Function: Get book from user
+Input: book directory
+Output: return a list of str of the book
+
+'''
 def get_book_text(userDirectory):
 
     with open(userDirectory) as f:
         
         # read book content
         book_content = f.read()
-        
+       
         # typecast to string 
         str(book_content)
 
         return book_content
+
+''' 
+Function: Give total word count of book
+Input: Book text
+Output: Total word count 
+'''
+def word_count(texts):
+
+    split = texts.split()  
+
+    print(f"Found {len(split)} total words")
 
 
 
@@ -17,9 +36,10 @@ def main():
     
     book_dir = "./books/frankenstein.txt"
 
-    print (get_book_text(book_dir))
-
+    word_count(get_book_text(book_dir))
     
+    
+
 
 
 
